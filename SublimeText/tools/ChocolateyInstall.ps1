3eaa8917adcde220db7e02fd124cc51676145c04
Alan Stevens <alanstevens@gmail.com>
2012-01-18 19:43:21 -0500

    Updated SublimeText to follow Jon Galloway's original package

--- SublimeText/tools/ChocolateyInstall.ps1
+++ SublimeText/tools/ChocolateyInstall.ps1
@@ -1,1 +1,7 @@
- Install-ChocolateyPackage 'SublimeText' 'MSI' '/quiet' 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202%20Build%202165%20Setup.exe' 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202%20Build%202165%20x64%20Setup.exe'
+$is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
+$installDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url = 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202%20Build%202165.zip'
+
+if ($is64bit) {$url = 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202%20Build%202165%20x64.zip'}
+
+Install-ChocolateyZipPackage 'sublimetext2' "$url" "$installDir"
