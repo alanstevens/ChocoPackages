9a7f78473ee958115c257dcd341e3bd95a36ba88
Alan Stevens <alanstevens@gmail.com>
2012-01-21 18:49:25 -0500

    renamed environment variable chocolatey_bin_path to chocolatey_bin_root

--- ActivePerl/tools/ChocolateyInstall.ps1
+++ ActivePerl/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'ActivePerl'
 $fileType = 'MSI'
 $binRoot = "$env:systemdrive\"
-if($env:chocolatey_bin_path -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_path}
+if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
 $silentArgs = '/quiet TARGETDIR="$binRoot" PERL_PATH=Yes PERL_EXT=Yes'
 $url = 'http://downloads.activestate.com/ActivePerl/releases/5.14.2.1402/ActivePerl-5.14.2.1402-MSWin32-x86-295342.msi'
 $url64bit = 'http://downloads.activestate.com/ActivePerl/releases/5.14.2.1402/ActivePerl-5.14.2.1402-MSWin32-x64-295342.msi'
--- Cygwin/tools/ChocolateyInstall.ps1
+++ Cygwin/tools/ChocolateyInstall.ps1
@@ -1,4 +1,6 @@
-$cygwinPath = Join-Path $env:SystemDrive 'cygwin'
+$binRoot = "$env:systemdrive\"
+if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+$cygwinPath = Join-Path $binRoot 'Cygwin'
 $packagePath = Join-Path $cygwinPath 'packages'
 Install-ChocolateyPackage 'Cygwin' 'EXE' "$('-n -q -R ' + $cygwinPath + ' -l ' + $packagePath + ' -s http://mirror.cs.vt.edu/pub/cygwin/cygwin/ -O -C Base, Shells
 #Accessibility,Admin,Archive,Audio,Base,Database,Devel,Doc,Editors,Gnome,Graphics,Interpreters,Libs,Mail,Math,Mingw,Net,Perl,Publishing,Python,Science,Security,Shells,System,Text,Utils,Web,X11')" 'http://www.cygwin.com/setup.exe'
