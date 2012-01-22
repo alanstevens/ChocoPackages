85c85426154e8d6afa442ddfa8f2961f93ea9a79
Alan Stevens <alanstevens@gmail.com>
2012-01-22 01:08:47 -0500

    Added perl icon and inf installer

--- ActivePerl/tools/ChocolateyInstall.ps1
+++ ActivePerl/tools/ChocolateyInstall.ps1
@@ -7,3 +7,15 @@ $url = 'http://downloads.activestate.com/ActivePerl/releases/5.14.2.1402/ActiveP
 $url64bit = 'http://downloads.activestate.com/ActivePerl/releases/5.14.2.1402/ActivePerl-5.14.2.1402-MSWin32-x64-295342.msi'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
+
+$toolsPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$parentPath = join-path $toolsPath '..'
+$contentPath = join-path $parentPath 'content'
+$infFile = join-path $contentPath 'PerlScriptIcon.inf'
+
+# Update the inf file with the content path
+Get-Content $infFile | Foreach-Object{$_ -replace "CONTENT_PATH", "$contentPath"} | Set-Content 'TempFile.txt'
+move-item 'TempFile.txt' $infFile -Force
+
+# install the inf file
+& rundll32 syssetup,SetupInfObjectInstallAction DefaultInstall 128 $infFile
