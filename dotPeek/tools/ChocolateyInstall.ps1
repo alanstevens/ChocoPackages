8ea97424fbb9a0eb37d83a0cd36dfca353475887
Alan Stevens <alanstevens@gmail.com>
2012-01-25 22:47:32 -0500

    added dotPeek package

---
+++ dotPeek/tools/ChocolateyInstall.ps1
@@ -0,0 +1,5 @@
+$packageName = 'dotPeek'
+$url = 'http://download-ln.jetbrains.com/dotpeek/dotPeek-1.0.0.7999.zip'
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage $packageName $url
