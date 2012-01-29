c891c7435bd459441327a38306afef528ce5b7df
Alan Stevens <alanstevens@gmail.com>
2012-01-28 20:31:57 -0500

    added xnews package

--- Xnews/tools/ChocolateyInstall.ps1
+++ Xnews/tools/ChocolateyInstall.ps1
@@ -1,10 +1,5 @@
-$packageName = ''
-$fileType = ''
-$silentArgs = ''
-$url = ''
-$url64bit = ''
-
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
+$packageName = 'Xnews'
+$url = 'http://xnews.newsguy.com/release/xnews.zip'
 
 $chocTempDir = Join-Path $env:TEMP "chocolatey"
 $tempDir = Join-Path $chocTempDir "$packageName"
@@ -17,9 +12,17 @@ $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
 $programFiles = $env:programfiles
 if ($is64bit) {$programFiles = ${env:ProgramFiles(x86)}}
 
-$executable = join-path $programfiles '.exe'
 
 $fsObject = New-Object -ComObject Scripting.FileSystemObject
+$programFiles = $fsObject.GetFolder("$programFiles").ShortPath
+$unzipLocation = join-path $programFiles 'Xnews'
+
+Start-ChocolateyProcessAsAdmin "Install-ChocolateyZipPackage $packageName $url `"$unzipLocation`""
+
+$executable = join-path $programfiles 'Xnews\Xnews.exe'
+
+Install-ChocolateyDesktopLink $executable
+
 $executable = $fsObject.GetFile("$executable").ShortPath
 
 #add batch file
