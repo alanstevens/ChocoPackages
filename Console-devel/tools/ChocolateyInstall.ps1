e3314e1a8e1c45025b4db39515dd42c5932f7ef1
Alan Stevens <alanstevens@gmail.com>
2012-01-30 17:07:01 -0500

    fixed issue with 64bit console installs and used a hardlink when available

--- Console-devel/tools/ChocolateyInstall.ps1
+++ Console-devel/tools/ChocolateyInstall.ps1
@@ -8,12 +8,20 @@ Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
 
 $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
 $console = join-path $env:programfiles 'Console\Console.exe'
-if ($is64bit) {$console = join-path  ${env:ProgramFiles(x86)} 'Console\Console.exe'}
+if ($is64bit) {$console = join-path  $env:ProgramW6432 'Console\Console.exe'}
 $fsObject = New-Object -ComObject Scripting.FileSystemObject
 $console = $fsObject.GetFile("$console").ShortPath
 
+$symLinkName = Join-Path $nugetExePath "Console.exe"
 $batchFileName = Join-Path $nugetExePath "Console.bat"
 
+if( (gwmi win32_operatingSystem).version -ge 6){
+  # delete the bat file if it exists.
+  if(Test-Path $batchFileName){
+    Remove-Item "$batchFileName"}
+  & $env:comspec /c mklink /H $symLinkName $console}
+else{
 "@echo off
 SET DIR=%~dp0%
 start $console %*" | Out-File $batchFileName -encoding ASCII
+    }
