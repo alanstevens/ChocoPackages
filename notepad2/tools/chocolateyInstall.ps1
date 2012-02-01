a543f2354eb297955c5677c4038b0792611f84b7
Alan Stevens <alanstevens@gmail.com>
2012-02-01 16:28:24 -0500

    Corrected Notepad2 silent install switch.

--- notepad2/tools/chocolateyInstall.ps1
+++ notepad2/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'notepad2'
 $fileType = 'exe'
-$silentArgs = '/S'
+$silentArgs = '/SILENT'
 $url = 'http://www.flos-freeware.ch/zip/Notepad2_4.2.25_x86.exe'
 $url64bit = 'http://www.flos-freeware.ch/zip/Notepad2_4.2.25_x64.exe'
 
