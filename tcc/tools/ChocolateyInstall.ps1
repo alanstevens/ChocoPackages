292bc719a9687a6bfd47b3f930bfc0bae9668e45
Alan Stevens <alanstevens@gmail.com>
2012-02-05 18:24:55 -0500

    Adding tcc package

---
+++ tcc/tools/ChocolateyInstall.ps1
@@ -0,0 +1,7 @@
+$packageName = 'tcc'
+$fileType = 'exe'
+$silentArgs = '/Q'
+$url = 'http://jpsoft.com/downloads/v13/tccle.exe'
+$url64bit = 'http://jpsoft.com/downloads/v13/tcclex64.exe'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
