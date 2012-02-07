8cf1709baa330d6c4555dd98a4e585a31ae69516
Alan Stevens <alanstevens@gmail.com>
2012-02-07 08:39:07 -0500

    Updated pidgen package to kill running pidgin processes before installing

--- Pidgin/tools/ChocolateyInstall.ps1
+++ Pidgin/tools/ChocolateyInstall.ps1
@@ -3,4 +3,6 @@ $fileType = 'exe'
 $silentArgs = '/S'
 $url = 'http://sourceforge.net/projects/pidgin/files/Pidgin/2.10.1/pidgin-2.10.1-offline.exe'
 
+stop-process -processname pidgin*
+
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
