6a90846e7cc3343e5b2d8032e29547ce6c8ef952
Alan Stevens <alanstevens@gmail.com>
2012-03-27 20:45:18 -0400

    adding more vs extensions

--- alanstevens.vsextensions/tools/ChocolateyInstall.ps1
+++ alanstevens.vsextensions/tools/ChocolateyInstall.ps1
@@ -46,6 +46,18 @@ try {
     installsilently http://visualstudiogallery.msdn.microsoft.com/6ed4c78f-a23e-49ad-b5fd-369af0c2107f/file/50769/31/WebEssentials.vsix WebEssentials.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/fa85b17d-3df2-49b1-bee6-71527ffef441/file/49766/1/PerfWatsonExtension-Signed.vsix PerfWatson.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/961e6734-cd3a-4afb-a121-4541742b912e/file/36823/21/JSLint.VS2010.vsix JSLint.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/01516103-d487-4a7e-bb40-c15ec709afa3/file/31518/9/PowerGUIVsx.vsix PowerGUIVsx.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/c8164c71-0836-4471-80ce-633383031099/file/9701/9/VisualNunitPackage.vsix VisualNunitPackage.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/7c8341f1-ebac-40c8-92c2-476db8d523ce/file/15808/9/SpellChecker.vsix SpellChecker.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/63a7e40d-4d71-4fbb-a23b-d262124b8f4c/file/29105/34/GitSccProvider.vsix GitSccProvider.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/7812b04c-db36-4817-8a84-e73c452410a2/file/56231/3/DbContextCSharp.vsix DbContextCSharp.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/20cd93a2-c435-4d00-a797-499f16402378/file/13848/1/ThemeManagerPackage.vsix ThemeManagerPackage.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/23df0450-5677-4926-96cc-173d02752313/file/15689/1/PocoCSharp.vsix PocoCSharp.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/e5f41ad9-4edc-4912-bca3-91147db95b99/file/7088/6/PowerCommands.vsix PowerCommands.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/a15c3ce9-f58f-42b7-8668-53f6cdc2cd83/file/52418/2/Web%20Standards%20Update.msi Web%20Standards%20Update.msi
+    installsilently http://visualstudiogallery.msdn.microsoft.com/d491911d-97f3-4cf6-87b0-6a2882120acf/file/25426/68/DPStudio.VSCommands.vsix DPStudio.VSCommands.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/27077b70-9dad-4c64-adcf-c7cf6bc9970c/file/37502/19/NuGet.Tools.vsix NuGet.Tools.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/63a7e40d-4d71-4fbb-a23b-d262124b8f4c/file/29105/34/GitSccProvider.vsix GitSccProvider
 
     Write-ChocolateySuccess $packageName
 } catch {
