e0ca37c1c33eb8c0c71be266a1ce6255a748bebc
Alan Stevens <alanstevens@gmail.com>
2012-06-27 22:22:34 -0400

    Updated SublimeText to RTM

--- SublimeText2/tools/ChocolateyInstall.ps1
+++ SublimeText2/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
 $installDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$url = 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202%20Build%202165.zip'
+$url = 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.zip'
 
-if ($is64bit) {$url = 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202%20Build%202165%20x64.zip'}
+if ($is64bit) {$url = 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0%20x64.zip'}
 
 Install-ChocolateyZipPackage 'sublimetext2' "$url" "$installDir"
