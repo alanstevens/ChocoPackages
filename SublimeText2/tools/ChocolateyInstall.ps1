51be1c6793a40a8c20d98ee83d26611790f7c781
Matt Wrock <matt@mattwrock.com>
2012-10-27 12:45:43 -0700

    syncing sublime with what is on chocolatey.org

--- SublimeText2/tools/ChocolateyInstall.ps1
+++ SublimeText2/tools/ChocolateyInstall.ps1
@@ -1,3 +1,1 @@
-Install-ChocolateyPackage 'sublimetext2' 'exe' '/silent' `
-    'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20Setup.exe'     `
-    'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64%20Setup.exe'
+Install-ChocolateyPackage 'sublimetext2' 'exe' '' 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20Setup.exe' 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64%20Setup.exe'
