0298e4f8b628e7bb6b8acfb3adcefb3814214b2a
Alan Stevens <alanstevens@gmail.com>
2012-11-01 15:16:20 -0400

    Fixed issue downloading IcoFX

--- IcoFx/tools/ChocolateyInstall.ps1
+++ IcoFx/tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'IcoFx'
 $fileType = 'exe'
 $silentArgs = '/silent'
-$url = 'http://files1.freewareupdate.com/192874754126375148716237182368754817281726815412512512314/images-photos/icofx-setup.exe'
+$url = 'http://software-files-a.cnet.com/s/software/12/79/26/23/icofxsetup.exe'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
