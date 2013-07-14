e86ff54a9743a87c27ec9088ec91cbb37e2a864f
Iristyle <Iristyle@Github>
2013-07-14 15:24:22 -0400

    chore: bump SublimeText2 to 2.0.2

--- SublimeText2/tools/ChocolateyInstall.ps1
+++ SublimeText2/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'sublimetext2' 'exe' '/VERYSILENT /NORESTART /TASKS="contextentry"' 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20Setup.exe' 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64%20Setup.exe'
+Install-ChocolateyPackage 'sublimetext2' 'exe' '/VERYSILENT /NORESTART /TASKS="contextentry"' 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20Setup.exe' 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64%20Setup.exe'
