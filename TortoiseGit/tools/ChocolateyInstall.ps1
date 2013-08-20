172506379f877ad04dd9f297c66dafefb76b4ae4
Pure Krome <github@world-domination.com.au>
2013-08-20 21:36:39 +1000

    Updated TortoiseGit NuGet spec and version to 1.8.5.

--- TortoiseGit/tools/ChocolateyInstall.ps1
+++ TortoiseGit/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'TortoiseGit' 'MSI' '/QN /norestart REBOOT=ReallySuppress' 'http://tortoisegit.googlecode.com/files/TortoiseGit-1.8.4.0-32bit.msi' 'http://tortoisegit.googlecode.com/files/TortoiseGit-1.8.4.0-64bit.msi'
+Install-ChocolateyPackage 'TortoiseGit' 'MSI' '/QN /norestart REBOOT=ReallySuppress' 'http://tortoisegit.googlecode.com/files/TortoiseGit-1.8.5.0-32bit.msi' 'http://tortoisegit.googlecode.com/files/TortoiseGit-1.8.5.0-64bit.msi'
