ea48ade7b7d26d8a8c3388689025291f53292fec
Lctrs <jerome.parmentier@acensi.fr>
2013-11-13 11:50:12 +0100

    WinSCP: install translation depending on user locale

--- WinSCP/tools/ChocolateyInstall.ps1
+++ WinSCP/tools/ChocolateyInstall.ps1
@@ -1,5 +1,11 @@
 $packageName = 'winscp'
-$url = 'http://prdownloads.sourceforge.net/winscp/winscp517.zip'
-$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url = 'http://prdownloads.sourceforge.net/winscp/winscp517setup.exe'
+$language = (Get-Culture).TwoLetterISOLanguageName
 
-Install-ChocolateyZipPackage $packageName $url $toolsDir
+if ($language -eq $null) {
+	$language = 'en' # Fallback language if others fail
+}
+
+$args = "/VERYSILENT /LANG=$language"
+
+Install-ChocolateyPackage $packageName 'exe' $args $url 
