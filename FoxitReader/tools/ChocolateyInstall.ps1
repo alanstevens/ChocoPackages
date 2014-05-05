7f59222a318f60159c20130a659bf8506f23f122
bspinner <bspinner@users.noreply.github.com>
2014-05-05 20:21:01 +0200

    Updated version and corrected argument

--- FoxitReader/tools/ChocolateyInstall.ps1
+++ FoxitReader/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'EnterpriseFoxitReader620.0429_enu.msi' 'msi' '/passive /norestart /qn' 'http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/win/6.x/6.2/enu/EnterpriseFoxitReader620.0429_enu.msi'
+Install-ChocolateyPackage 'FoxitReader620.0429_enu_Setup.exe' 'EXE' '/verysilent' 'http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/win/6.x/6.2/enu/FoxitReader620.0429_enu_Setup.exe'
