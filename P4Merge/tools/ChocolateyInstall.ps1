6f55e55c4c46772cad5bcb53db930bc6cec112ea
Jeff Moser <jeff@moserware.com>
2014-06-02 08:41:55 -0400

    Updated p4merge package to 2014.1

--- P4Merge/tools/ChocolateyInstall.ps1
+++ P4Merge/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'P4Merge' 'EXE' '/v"/qn ADDLOCAL=P4MERGE"' 'http://www.perforce.com/downloads/perforce/r13.2/bin.ntx86/p4vinst.exe' 'http://www.perforce.com/downloads/perforce/r13.2/bin.ntx64/p4vinst64.exe'
+Install-ChocolateyPackage 'P4Merge' 'EXE' '/v"/qn ADDLOCAL=P4MERGE"' 'http://filehost.perforce.com/perforce/r14.1/bin.ntx86/p4vinst.exe' 'http://filehost.perforce.com/perforce/r14.1/bin.ntx64/p4vinst64.exe'
