5129ea9c54a010b78208f0089a50f463665ac95a
mew1033 <chandler.newby@gmail.com>
2014-11-03 15:57:12 -0700

    Added ChocolateyUninstall.ps1 for WizMouse

---
+++ WizMouse/tools/chocolateyUninstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName = 'wizmouse'
+$installerType = 'EXE'
+$silentArgs = '/VERYSILENT /SP- /SUPPRESSMSGBOXES /NORESTART'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+$unfolder = "WizMouse" #Name of the folder here. No slashs.
+$unfile = "unins000.exe" #Put the name of the uninstall file (with the extension) here. Example: unins000.exe
+
+
+try {
+  if (Test-Path "${Env:ProgramFiles(x86)}\$unfolder") {
+    $unpath = "${Env:ProgramFiles(x86)}\$unfolder\$unfile"
+  } else {
+    $unpath = "$Env:ProgramFiles\$unfolder\$unfile"
+  }
+  Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$unpath" -validExitCodes $validExitCodes
+   
+  # the following is all part of error handling
+  # Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
