88d054f96a07eeb0a745a3676d4c91551eea03d3
TomOne <TomOne@users.noreply.github.com>
2015-02-03 21:05:48 +0100

    The BinRoot package is deprecated. Better remove it.
    The BinRoot functionality is already built into Chocolatey.

--- BinRoot/tools/ChocolateyInstall.ps1
+++
@@ -1,5 +0,0 @@
-$packageName = 'BinRoot'
-
-### Set the environment variable for the bin root to a default value if not already set ###
-if($env:chocolatey_bin_root -eq $null){[Environment]::SetEnvironmentVariable("chocolatey_bin_root", "C:\bin", "User")}
-if($env:ChocolateyBinRoot -eq $null){[Environment]::SetEnvironmentVariable("ChocolateyBinRoot", "C:\bin", "User")}
