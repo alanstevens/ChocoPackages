0d3f1fea51d914956c5c497bf1340cb9de19e968
Alan Stevens <alanstevens@gmail.com>
2015-04-07 18:17:23 -0400

    Remove FoxitReader as it is maintained in @ComFreek's repo.

--- FoxitReader/tools/ChocolateyInstall.ps1
+++
@@ -1,1 +0,0 @@
-Install-ChocolateyPackage 'FoxitReader620.0429_enu_Setup.exe' 'EXE' '/verysilent' 'http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/win/6.x/6.2/enu/FoxitReader620.0429_enu_Setup.exe'
