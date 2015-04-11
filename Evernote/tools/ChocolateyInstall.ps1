09572af241d0069b17672e88a347d5b301583de2
TomOne <TomOne@users.noreply.github.com>
2015-04-11 16:59:00 +0200

    Remove packages that are maintained by somebody else

--- Evernote/tools/ChocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = 'Evernote'
-$installerType = 'exe'
-$silentArgs = '/quiet'
-$url = 'http://evernote.com/download/get.php?file=Win' # No 64-bit version available
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
--- Evernote/tools/chocolateyUninstall.ps1
+++
@@ -1,3 +0,0 @@
-# Msiexec really doesn't work too well when called from PowerShell.  This avoids the issue by getting cmd.exe to do the hard work.
-$uninstallPath = 'cmd /c "' + (Get-ItemProperty "HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{32D39568-3B77-11E3-88CE-00163E98E7D0}" UninstallString).UninstallString + ' /quiet"'
-Invoke-Expression $uninstallPath
