b077ae611d8a5717844e0174840288ca49fcee35
Patrick Wyatt <pat@codeofhonor.com>
2019-11-15 18:43:18 -0800

    Download installer from Github repository
    The Growl installer is no longer available at www.growlforwindows.com.
    Download the installer from the Github repository created by
    Brian Dunnington instead.
    
    Minor updates to the installation script to remove the use of
    Write-ChocolateySuccess and Write-ChocolateyFailure, which are
    deprecated.

--- Growl/tools/ChocolateyInstall.ps1
+++ Growl/tools/ChocolateyInstall.ps1
@@ -1,33 +1,20 @@
-$packageName = 'Growl'
-
-try
+if ([Environment]::OSVersion.Version -gt '6.2')
 {
-    if ([Environment]::OSVersion.Version -gt '6.2')
+    $feature = @{FeatureName = 'NetFx3'; Online = $true}
+    $fxInstalled = Get-WindowsOptionalFeature @feature |
+        Select -ExpandProperty State
+
+    if ($fxInstalled -ne 'Enabled')
     {
-        $feature = @{FeatureName = 'NetFx3'; Online = $true}
-        $fxInstalled = Get-WindowsOptionalFeature @feature |
-            Select -ExpandProperty State
-
-        if ($fxInstalled -ne 'Enabled')
-        {
-            Write-Host 'Enabling .NET Framework 2.0 runtime...'
-            Enable-WindowsOptionalFeature @feature
-        }
+        Write-Host 'Enabling .NET Framework 2.0 runtime...'
+        Enable-WindowsOptionalFeature @feature
     }
-
-    $params = @{
-        packageName = $package;
-        fileType = 'exe';
-        silentArgs = '/c:"msiexec -i Growl_v2.0.msi /qn"'
-        url = 'http://www.growlforwindows.com/gfw/d.ashx?f=GrowlInstaller.exe';
-    }
-
-    Install-ChocolateyPackage @params
-
-    Write-ChocolateySuccess $packageName
 }
-catch
-{
-    Write-ChocolateyFailure $package "$($_.Exception.Message)"
-    throw
+
+$params = @{
+    packageName = "growl";
+    fileType = 'exe';
+    silentArgs = '/c:"msiexec -i Growl_v2.0.msi /qn"';
+    url = 'https://github.com/briandunnington/growl-for-windows/releases/download/final/GrowlInstaller.exe';
 }
+Install-ChocolateyPackage @params
