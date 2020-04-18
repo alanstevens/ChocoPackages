920c176412ac7eafb38ac7da06a9c821cbf6a4e1
mark05E <mark05e@users.noreply.github.com>
2020-04-18 16:08:49 +0300

    Executor: Update checksum for v1.0.0
    Executor: Update checksum for v1.0.0

--- Executor/tools/ChocolateyInstall.ps1
+++ Executor/tools/ChocolateyInstall.ps1
@@ -4,9 +4,9 @@ $packageArgs = @{
     softwareName = 'Executor'
     packageName = 'executor'
     silentArgs = '/verysilent'
-    checksum      = '10D170EEBE36D0E43F2A2BDE0F7E3712BB82AC8141D60126BDEBF67A3F191B79'
+    checksum      = '4F9666D647019A488E7959831505EA0C05083A60E0D33F191C25BC872E419BDD'
     checksumType  = 'sha256'
-    checksum64    = '10D170EEBE36D0E43F2A2BDE0F7E3712BB82AC8141D60126BDEBF67A3F191B79'
+    checksum64    = '4F9666D647019A488E7959831505EA0C05083A60E0D33F191C25BC872E419BDD'
     checksumType64= 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
